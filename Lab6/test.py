907226f0eba16d3e5d65185ce819f1be5e27cc47
perapatwan-blip <perapat.wan@spumail.net>
2026-02-17 12:42:52 +0700

    Add files via upload

---
+++ Lab6/test.py
@@ -0,0 +1,10 @@
+print("Hello Python")
+
+age = 25
+print("อายุของฉันคือ", age)
+
+name = "Perapat Wanna"  
+age = 20
+print(f"สวัสดี {name}, อายุ {age} ปี")
+
+print("Python", "Java", "C++", sep=" | ")
